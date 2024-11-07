95bbf9ca23313c29d5a8eba529e51d60e733837c
sofia357 <dahenkosofia1908@gmail.com>
2024-11-07 16:00:49 +0200

    sofia

---
+++ target/debug/deps/utf8_slice-b7e992c221d0bb39.d
@@ -0,0 +1,7 @@
+C:\Users\New\RustroverProjects\rust-221on\target\debug\deps\libutf8_slice-b7e992c221d0bb39.rmeta: C:\Users\New\.cargo\registry\src\index.crates.io-6f17d22bba15001f\utf8_slice-1.0.0\src\lib.rs
+
+C:\Users\New\RustroverProjects\rust-221on\target\debug\deps\libutf8_slice-b7e992c221d0bb39.rlib: C:\Users\New\.cargo\registry\src\index.crates.io-6f17d22bba15001f\utf8_slice-1.0.0\src\lib.rs
+
+C:\Users\New\RustroverProjects\rust-221on\target\debug\deps\utf8_slice-b7e992c221d0bb39.d: C:\Users\New\.cargo\registry\src\index.crates.io-6f17d22bba15001f\utf8_slice-1.0.0\src\lib.rs
+
+C:\Users\New\.cargo\registry\src\index.crates.io-6f17d22bba15001f\utf8_slice-1.0.0\src\lib.rs:
